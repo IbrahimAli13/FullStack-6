f56287b73d746b6d8fa667ec7de6a7d485c6c7b7
IbrahimAli13 <ibraheemsaid200@gmail.com>
2023-08-03 23:02:27 +0300

    Update

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function changeColor(color) {
-    document.body.style.backgroundColor = color;
-    const sloganImg = document.querySelector('.slogan-container img');
-    sloganImg.src = `pepsi-can-${color}.png`;
-  }
-  
+  document.body.style.backgroundColor = color;
+  const sloganImg = document.querySelector('.slogan-container img');
+  sloganImg.src = `pepsi-can-${color}.png`;
+
+  const header = document.querySelector('header');
+  header.style.backgroundColor = color;
+
+  const footer = document.querySelector('footer');
+  footer.style.backgroundColor = color;
+}
